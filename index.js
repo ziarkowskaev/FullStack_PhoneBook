fd9429d7ad26a407f14d2fdc96f5d00c4e8e3544
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-01-05 17:49:49 +0200

    for deployment

--- index.js
+++ index.js
@@ -1,108 +1,118 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+
+
+const Person = require('./models/persons')
 
 app.use(express.static('dist'))
 app.use(express.json())
+
 app.use(cors())
 // create "middleware"
 
-//create a token 
-morgan.token('data', function(req, res) {
-        return JSON.stringify(req.body); 
-});
 
-//custom "tiny"
-app.use(
-    morgan(':method :url :status :res[content-length] - :response-time ms :data', {
-        skip: function(req, res) {return req.method !== 'POST' } // skip if method is not POST
-    }));
+//3.16
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+      }
+  
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
 ]
 
-app.get('/info', (request, response) => {
-    const num = persons.length
+//3.18
+app.get('/info', async (request, response) => {
+    const num = await Person.countDocuments({})
     const date = new Date().toString()
     response.send(`<p>Phonebook has info for ${num} people</p></br><p>${date}</p>`)
   })
 
-  
+//3.13 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+
+    Person.find({}).then(result => {
+        response.json(result)
+        })
+        
 })
 
-app.get('/api/persons/:id', (request, response) => {
-
-    const id = Number(request.params.id)
-    const person = persons.find(note => note.id === id)
-    
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
-    response.status(204).end()
-})
-
-  app.post('/api/persons', (request, response) => {
+//3.14
+app.post('/api/persons', (request, response, next) => {
 
     const body = request.body
 
-    console.log(body.name)
+    const person = new Person({
+        name: body.name, 
+        number: body.number
+    })
   
-    if (!body.name || !body.number) {
-      return response.status(400).json({ 
-        error: 'The name or number is missing' 
+    person.save()
+        .then(result => {
+            console.log(`added ${person.name} number ${person.number} to phonebook!`)
+            response.json(result)
       })
-    }
-
-    if (persons.some(person => person.name === body.name)) {
-        return response.status(400).json({ 
-          error: 'The name already exists in the phonebook' 
-        })
-      }
-  
-    const person = {
-        "id": Math.floor(Math.random() * 500),
-        "name": body.name, 
-        "number": body.number
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+      .catch(error => next(error))
   })
 
-  const PORT = process.env.PORT || 3001
+
+//3.18
+app.get('/api/persons/:id', (request, response, next) => {
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+//3.15 
+app.delete('/api/persons/:id', (request, response,next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(() => {
+            response.status(204).end()
+            })
+            .catch(error => next(error))
+})
+
+
+  //3.17
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+        name: body.name, 
+        number: body.number
+    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
+    app.use(unknownEndpoint)
+    app.use(errorHandler)
+
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
---
+++ models/persons.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const url = process.env.MONGODB_URI
+
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+//define person
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+      },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+              return /\d{2,3}-\d{6,}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+          },
+        required: true
+      }
+  })
+  
+  //transform schema
+  personSchema.set('toJSON', {
+      transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+      }
+  })
+
+
+module.exports = mongoose.model('Person', personSchema)
